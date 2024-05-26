e50d31c63ac43f82c87565cda0c9196018ce082b
Анна Терентьева <annyshka20072002@gmail.com>
2024-05-26 04:27:44 +0300

    result automaton min view

--- converter/src/tex_parser.py
+++ converter/src/tex_parser.py
@@ -53,18 +53,19 @@ def tex_parse(text):
         line = lines[i]
         if not line.isspace() and line:
             if re.search(r"\\section{.*}", line):
-                res.append(create_tag('h3', re.findall(r"\\section{(.*)}", line)[0] + ':'))
+                res.append({'type': 'section', 'res': create_tag('h3', re.findall(r"\\section{(.*)}", line)[0] + ':')})
             elif "\\begin{tikzpicture}" in line:
                 graph_tex = ""
                 while "\\end{tikzpicture}" not in line and i < len(lines):
                     i += 1
                     line = lines[i]
                     graph_tex += line + '\n'
-                res.append(create_tag('pre', graph_tex))
+                format_list = [{'name': 'LaTeX', 'txt': graph_tex}, {'name': 'for-test', 'txt': 'test txt'}]
+                res.append({'type': 'automaton', 'res': format_list})
             else:
                 print(repr(line))
                 line = apply_mathml(line)
-                res.append(create_tag('p', line))
+                res.append({'type': 'text', 'res': create_tag('p', line)})
         i += 1
 
     return res
